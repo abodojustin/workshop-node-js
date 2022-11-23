71be04efee8759ba38d03540d43c54c412703f44
jafacorp <56782606+abodojustin@users.noreply.github.com>
2022-11-23 17:16:26 +0100

    setup node js project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+let port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port} via http://localhost:${port}`);
+});
